167f18e5592b49be6f81677aab346c4e4b1d1756
qiukuip <zhulongkun20@icloud.com>
2026-09-06 18:59:30 +0800

    Update train path to runs/detect/train, add validate_onnx_fp16.py, track best_int8.onnx

--- validate_onnx_int8.py
+++ validate_onnx_int8.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
 
-model = YOLO('runs/detect/train-2/weights/best_int8.onnx') 
+model = YOLO('runs/detect/train/weights/best_int8.onnx') 
 
 results = model.predict('model-val-1.jpeg')
 
